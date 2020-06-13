23ecefc8e03f4bd126f2f190a200b26af1dc8bab
Shevchuk-Oleksandr <shevchuk_od@ukr.net>
2020-06-13 20:36:44 +0300

    z

--- js/modal.js
+++ js/modal.js
@@ -9,30 +9,6 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('backdrop---is-hidden');
+    refs.modal.classList.toggle('is-hidden');
   }
-})();
-
-(() => {
-  document.querySelector('.subscription-form').addEventListener('submit', e => {
-    e.preventDefault();
-
-    new FormData(e.currentTarget).forEach((value, name) =>
-      console.log(`${name}: ${value}`),
-    );
-
-    e.currentTarget.reset();
-  });
-})();
-
-(() => {
-  document.querySelector('.callback-form').addEventListener('submit', e => {
-    e.preventDefault();
-
-    new FormData(e.currentTarget).forEach((value, name) =>
-      console.log(`${name}: ${value}`),
-    );
-
-    e.currentTarget.reset();
-  });
 })();
